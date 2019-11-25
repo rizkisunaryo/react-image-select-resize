48f80258207a63cdb68977cd96b94e3409e69e76
Rizki Sunaryo <rizkisunaryo@gmail.com>
2019-11-25 15:56:49 +0700

    use React , and import ReactDOM

--- index.js
+++ index.js
@@ -1,14 +1,113 @@
-!function(e){var t={};function r(n){if(t[n])return t[n].exports;var o=t[n]={i:n,l:!1,exports:{}};return e[n].call(o.exports,o,o.exports,r),o.l=!0,o.exports}r.m=e,r.c=t,r.d=function(e,t,n){r.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:n})},r.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},r.t=function(e,t){if(1&t&&(e=r(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var n=Object.create(null);if(r.r(n),Object.defineProperty(n,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)r.d(n,o,function(t){return e[t]}.bind(null,o));return n},r.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return r.d(t,"a",t),t},r.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},r.p="",r(r.s=1)}([function(e,t,r){"use strict";e.exports=r(2)},function(e,t,r){"use strict";r.r(t);var n=r(0),o=r.n(n);t.default=(e={})=>{const t=Object(n.useRef)(),r=(t,r)=>{"function"==typeof e.onImageSelected&&e.onImageSelected(t),"function"==typeof e.onImageSelectedUrl&&e.onImageSelectedUrl(r)};return o.a.createElement("div",{style:e.style,onClick:()=>{t&&t.current&&"function"==typeof t.current.click&&t.current.click()}},e.children,o.a.createElement("input",{onChange:t=>{if(!e)return;const n=t.target.files[0],o=URL.createObjectURL(n);if(r(n,o),"function"==typeof e.onImageResized||"function"==typeof e.onImageResizedUrl){const t=e.maxWidth||800,u=e.maxHeight||600,c=n.name,i=new FileReader;i.readAsDataURL(n),i.onload=f=>{const l=new Image;l.src=f.target.result,l.onload=()=>{if(l.width<=t&&l.height<=u)return void r(n,o);const i=t/u,f=l.width/l.height;let a=t,s=u;f>i?s=a/f:f<i&&(a=s*f);const p=document.createElement("canvas");p.width=a,p.height=s;const y=p.getContext("2d");y.drawImage(l,0,0,a,s),y.canvas.toBlob(t=>{const r=new File([t],c,{type:"image/jpeg",lastModified:Date.now()});((t,r)=>{"function"==typeof e.onImageResized&&e.onImageResized(t),"function"==typeof e.onImageResizedUrl&&e.onImageResizedUrl(r)})(r,URL.createObjectURL(r))},"image/jpeg",1)},i.onerror=e=>console.log(e)}}},type:"file",ref:t,hidden:!0,accept:"image/png, image/jpeg"}))}},function(e,t,r){"use strict";
-/** @license React v16.12.0
- * react.production.min.js
- *
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */var n=r(3),o="function"==typeof Symbol&&Symbol.for,u=o?Symbol.for("react.element"):60103,c=o?Symbol.for("react.portal"):60106,i=o?Symbol.for("react.fragment"):60107,f=o?Symbol.for("react.strict_mode"):60108,l=o?Symbol.for("react.profiler"):60114,a=o?Symbol.for("react.provider"):60109,s=o?Symbol.for("react.context"):60110,p=o?Symbol.for("react.forward_ref"):60112,y=o?Symbol.for("react.suspense"):60113;o&&Symbol.for("react.suspense_list");var d=o?Symbol.for("react.memo"):60115,m=o?Symbol.for("react.lazy"):60116;o&&Symbol.for("react.fundamental"),o&&Symbol.for("react.responder"),o&&Symbol.for("react.scope");var b="function"==typeof Symbol&&Symbol.iterator;function h(e){for(var t="https://reactjs.org/docs/error-decoder.html?invariant="+e,r=1;r<arguments.length;r++)t+="&args[]="+encodeURIComponent(arguments[r]);return"Minified React error #"+e+"; visit "+t+" for the full message or use the non-minified dev environment for full errors and additional helpful warnings."}var g={isMounted:function(){return!1},enqueueForceUpdate:function(){},enqueueReplaceState:function(){},enqueueSetState:function(){}},v={};function S(e,t,r){this.props=e,this.context=t,this.refs=v,this.updater=r||g}function j(){}function w(e,t,r){this.props=e,this.context=t,this.refs=v,this.updater=r||g}S.prototype.isReactComponent={},S.prototype.setState=function(e,t){if("object"!=typeof e&&"function"!=typeof e&&null!=e)throw Error(h(85));this.updater.enqueueSetState(this,e,t,"setState")},S.prototype.forceUpdate=function(e){this.updater.enqueueForceUpdate(this,e,"forceUpdate")},j.prototype=S.prototype;var _=w.prototype=new j;_.constructor=w,n(_,S.prototype),_.isPureReactComponent=!0;var O={current:null},R={current:null},k=Object.prototype.hasOwnProperty,x={key:!0,ref:!0,__self:!0,__source:!0};function C(e,t,r){var n,o={},c=null,i=null;if(null!=t)for(n in void 0!==t.ref&&(i=t.ref),void 0!==t.key&&(c=""+t.key),t)k.call(t,n)&&!x.hasOwnProperty(n)&&(o[n]=t[n]);var f=arguments.length-2;if(1===f)o.children=r;else if(1<f){for(var l=Array(f),a=0;a<f;a++)l[a]=arguments[a+2];o.children=l}if(e&&e.defaultProps)for(n in f=e.defaultProps)void 0===o[n]&&(o[n]=f[n]);return{$$typeof:u,type:e,key:c,ref:i,props:o,_owner:R.current}}function E(e){return"object"==typeof e&&null!==e&&e.$$typeof===u}var P=/\/+/g,$=[];function I(e,t,r,n){if($.length){var o=$.pop();return o.result=e,o.keyPrefix=t,o.func=r,o.context=n,o.count=0,o}return{result:e,keyPrefix:t,func:r,context:n,count:0}}function U(e){e.result=null,e.keyPrefix=null,e.func=null,e.context=null,e.count=0,10>$.length&&$.push(e)}function A(e,t,r){return null==e?0:function e(t,r,n,o){var i=typeof t;"undefined"!==i&&"boolean"!==i||(t=null);var f=!1;if(null===t)f=!0;else switch(i){case"string":case"number":f=!0;break;case"object":switch(t.$$typeof){case u:case c:f=!0}}if(f)return n(o,t,""===r?"."+L(t,0):r),1;if(f=0,r=""===r?".":r+":",Array.isArray(t))for(var l=0;l<t.length;l++){var a=r+L(i=t[l],l);f+=e(i,a,n,o)}else if(null===t||"object"!=typeof t?a=null:a="function"==typeof(a=b&&t[b]||t["@@iterator"])?a:null,"function"==typeof a)for(t=a.call(t),l=0;!(i=t.next()).done;)f+=e(i=i.value,a=r+L(i,l++),n,o);else if("object"===i)throw n=""+t,Error(h(31,"[object Object]"===n?"object with keys {"+Object.keys(t).join(", ")+"}":n,""));return f}(e,"",t,r)}function L(e,t){return"object"==typeof e&&null!==e&&null!=e.key?function(e){var t={"=":"=0",":":"=2"};return"$"+(""+e).replace(/[=:]/g,(function(e){return t[e]}))}(e.key):t.toString(36)}function M(e,t){e.func.call(e.context,t,e.count++)}function z(e,t,r){var n=e.result,o=e.keyPrefix;e=e.func.call(e.context,t,e.count++),Array.isArray(e)?q(e,n,r,(function(e){return e})):null!=e&&(E(e)&&(e=function(e,t){return{$$typeof:u,type:e.type,key:t,ref:e.ref,props:e.props,_owner:e._owner}}(e,o+(!e.key||t&&t.key===e.key?"":(""+e.key).replace(P,"$&/")+"/")+r)),n.push(e))}function q(e,t,r,n,o){var u="";null!=r&&(u=(""+r).replace(P,"$&/")+"/"),A(e,z,t=I(t,u,n,o)),U(t)}function F(){var e=O.current;if(null===e)throw Error(h(321));return e}var D={Children:{map:function(e,t,r){if(null==e)return e;var n=[];return q(e,n,null,t,r),n},forEach:function(e,t,r){if(null==e)return e;A(e,M,t=I(null,null,t,r)),U(t)},count:function(e){return A(e,(function(){return null}),null)},toArray:function(e){var t=[];return q(e,t,null,(function(e){return e})),t},only:function(e){if(!E(e))throw Error(h(143));return e}},createRef:function(){return{current:null}},Component:S,PureComponent:w,createContext:function(e,t){return void 0===t&&(t=null),(e={$$typeof:s,_calculateChangedBits:t,_currentValue:e,_currentValue2:e,_threadCount:0,Provider:null,Consumer:null}).Provider={$$typeof:a,_context:e},e.Consumer=e},forwardRef:function(e){return{$$typeof:p,render:e}},lazy:function(e){return{$$typeof:m,_ctor:e,_status:-1,_result:null}},memo:function(e,t){return{$$typeof:d,type:e,compare:void 0===t?null:t}},useCallback:function(e,t){return F().useCallback(e,t)},useContext:function(e,t){return F().useContext(e,t)},useEffect:function(e,t){return F().useEffect(e,t)},useImperativeHandle:function(e,t,r){return F().useImperativeHandle(e,t,r)},useDebugValue:function(){},useLayoutEffect:function(e,t){return F().useLayoutEffect(e,t)},useMemo:function(e,t){return F().useMemo(e,t)},useReducer:function(e,t,r){return F().useReducer(e,t,r)},useRef:function(e){return F().useRef(e)},useState:function(e){return F().useState(e)},Fragment:i,Profiler:l,StrictMode:f,Suspense:y,createElement:C,cloneElement:function(e,t,r){if(null==e)throw Error(h(267,e));var o=n({},e.props),c=e.key,i=e.ref,f=e._owner;if(null!=t){if(void 0!==t.ref&&(i=t.ref,f=R.current),void 0!==t.key&&(c=""+t.key),e.type&&e.type.defaultProps)var l=e.type.defaultProps;for(a in t)k.call(t,a)&&!x.hasOwnProperty(a)&&(o[a]=void 0===t[a]&&void 0!==l?l[a]:t[a])}var a=arguments.length-2;if(1===a)o.children=r;else if(1<a){l=Array(a);for(var s=0;s<a;s++)l[s]=arguments[s+2];o.children=l}return{$$typeof:u,type:e.type,key:c,ref:i,props:o,_owner:f}},createFactory:function(e){var t=C.bind(null,e);return t.type=e,t},isValidElement:E,version:"16.12.0",__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED:{ReactCurrentDispatcher:O,ReactCurrentBatchConfig:{suspense:null},ReactCurrentOwner:R,IsSomeRendererActing:{current:!1},assign:n}},T={default:D},N=T&&D||T;e.exports=N.default||N},function(e,t,r){"use strict";
-/*
-object-assign
-(c) Sindre Sorhus
-@license MIT
-*/var n=Object.getOwnPropertySymbols,o=Object.prototype.hasOwnProperty,u=Object.prototype.propertyIsEnumerable;function c(e){if(null==e)throw new TypeError("Object.assign cannot be called with null or undefined");return Object(e)}e.exports=function(){try{if(!Object.assign)return!1;var e=new String("abc");if(e[5]="de","5"===Object.getOwnPropertyNames(e)[0])return!1;for(var t={},r=0;r<10;r++)t["_"+String.fromCharCode(r)]=r;if("0123456789"!==Object.getOwnPropertyNames(t).map((function(e){return t[e]})).join(""))return!1;var n={};return"abcdefghijklmnopqrst".split("").forEach((function(e){n[e]=e})),"abcdefghijklmnopqrst"===Object.keys(Object.assign({},n)).join("")}catch(e){return!1}}()?Object.assign:function(e,t){for(var r,i,f=c(e),l=1;l<arguments.length;l++){for(var a in r=Object(arguments[l]))o.call(r,a)&&(f[a]=r[a]);if(n){i=n(r);for(var s=0;s<i.length;s++)u.call(r,i[s])&&(f[i[s]]=r[i[s]])}}return f}}]);
+import React, { useRef } from 'react'
+import ReactDOM from 'react-dom';
+
+const ImageSelectResize = (props = {}) => {
+  const inputFile = useRef()
+
+  const returnOriginalImage = (file, fileUrl) => {
+    if (typeof props.onImageSelected === 'function') {
+      props.onImageSelected(file)
+    }
+    if (typeof props.onImageSelectedUrl === 'function') {
+      props.onImageSelectedUrl(fileUrl)
+    }
+  }
+
+  const returnResizedImage = (file, fileUrl) => {
+    if (typeof props.onImageResized === 'function') {
+      props.onImageResized(file)
+    }
+    if (typeof props.onImageResizedUrl === 'function') {
+      props.onImageResizedUrl(fileUrl)
+    }
+  }
+
+  return (
+    <div
+      style={props.style}
+      onClick={() => {
+        if (
+          inputFile &&
+          inputFile.current &&
+          typeof inputFile.current.click === 'function'
+        ) {
+          inputFile.current.click()
+        }
+      }}
+    >
+      {props.children}
+      <input
+        onChange={e => {
+          if (!props) return
+
+          const file = e.target.files[0]
+          const fileUrl = URL.createObjectURL(file)
+
+          returnOriginalImage(file, fileUrl)
+
+          if (
+            typeof props.onImageResized === 'function' ||
+            typeof props.onImageResizedUrl === 'function'
+          ) {
+            const maxWidth = props.maxWidth || 800
+            const maxHeight = props.maxHeight || 600
+
+            const fileName = file.name
+            const reader = new FileReader()
+            reader.readAsDataURL(file)
+            reader.onload = event => {
+              const img = new Image()
+              img.src = event.target.result
+              img.onload = () => {
+                if (img.width <= maxWidth && img.height <= maxHeight) {
+                  returnOriginalImage(file, fileUrl)
+                  return
+                }
+
+                const resizerRatio = maxWidth / maxHeight
+                const originalRatio = img.width / img.height
+
+                let width = maxWidth
+                let height = maxHeight
+                if (originalRatio > resizerRatio) {
+                  // modify height
+                  height = width / originalRatio
+                } else if (originalRatio < resizerRatio) {
+                  // modify width
+                  width = height * originalRatio
+                }
+
+                const elem = document.createElement('canvas')
+                elem.width = width
+                elem.height = height
+                const ctx = elem.getContext('2d')
+
+                // img.width and img.height will contain the original dimensions
+                ctx.drawImage(img, 0, 0, width, height)
+                ctx.canvas.toBlob(
+                  blob => {
+                    const resizedFile = new File([blob], fileName, {
+                      type: 'image/jpeg',
+                      lastModified: Date.now()
+                    })
+                    const resizedFileUrl = URL.createObjectURL(resizedFile)
+                    returnResizedImage(resizedFile, resizedFileUrl)
+                  },
+                  'image/jpeg',
+                  1
+                )
+              }
+              reader.onerror = error => console.log(error)
+            }
+          }
+        }}
+        type='file'
+        ref={inputFile}
+        hidden
+        accept='image/png, image/jpeg'
+      />
+    </div>
+  )
+}
+
+export default ImageSelectResize
